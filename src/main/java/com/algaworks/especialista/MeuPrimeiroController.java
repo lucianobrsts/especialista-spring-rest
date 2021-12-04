20035339ca0fb3853bc668cb4896120110d04f13
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-12-03 23:53:15 -0300

    Adicionado primeiro controller da applicação

---
+++ src/main/java/com/algaworks/especialista/MeuPrimeiroController.java
@@ -0,0 +1,15 @@
+package com.algaworks.especialista;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MeuPrimeiroController {
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "Olá você está no curso de Spring Rest";
+	}
+}
