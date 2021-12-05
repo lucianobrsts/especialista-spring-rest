4e27d740ae9d05cf1d335b25d4cf45b5d5089a1f
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-12-04 23:47:46 -0300

    Adicionado injeção usando @Component

--- src/main/java/com/algaworks/especialista/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/especialista/service/AtivacaoClienteService.java
@@ -1,19 +1,22 @@
 package com.algaworks.especialista.service;
 
+import org.springframework.stereotype.Component;
+
 import com.algaworks.especialista.modelo.Cliente;
 import com.algaworks.especialista.notificacao.Notificador;
 
+@Component
 public class AtivacaoClienteService {
-	
-private Notificador notificador;
-	
+
+	private Notificador notificador;
+
 	public AtivacaoClienteService(Notificador notificador) {
 		this.notificador = notificador;
 	}
-	
+
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
-		
+
 		this.notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
 
