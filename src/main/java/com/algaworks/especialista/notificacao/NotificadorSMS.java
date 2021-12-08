4fca233e13857f1d35f723b8c95fd53743c44800
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-12-08 07:51:59 -0300

    Resolvendo ambiguidade de beans

--- src/main/java/com/algaworks/especialista/notificacao/NotificadorSMS.java
+++ src/main/java/com/algaworks/especialista/notificacao/NotificadorSMS.java
@@ -9,8 +9,8 @@ public class NotificadorSMS implements Notificador {
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
-				cliente.getNome(), cliente.getEmail(), mensagem);
+		System.out.printf("Notificando %s através do telefone %s: %s\n", 
+				cliente.getNome(), cliente.getTelefone(), mensagem);
 	}
 
 }
