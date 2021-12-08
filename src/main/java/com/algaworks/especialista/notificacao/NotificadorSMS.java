403418156a6d17e48cd9dd8971647a3354386d3f
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-12-08 07:42:31 -0300

    Adicionado notificadorSMS ao projeto

---
+++ src/main/java/com/algaworks/especialista/notificacao/NotificadorSMS.java
@@ -0,0 +1,16 @@
+package com.algaworks.especialista.notificacao;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.especialista.modelo.Cliente;
+
+@Component
+public class NotificadorSMS implements Notificador {
+
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
+				cliente.getNome(), cliente.getEmail(), mensagem);
+	}
+
+}
