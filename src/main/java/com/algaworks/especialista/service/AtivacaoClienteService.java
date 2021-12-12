821069a3cbd2400be1ce356cc02044d4560b5d79
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-12-12 10:04:59 -0300

    definindo metodos init e destroy

--- src/main/java/com/algaworks/especialista/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/especialista/service/AtivacaoClienteService.java
@@ -1,24 +1,33 @@
 package com.algaworks.especialista.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.algaworks.especialista.modelo.Cliente;
 import com.algaworks.especialista.notificacao.NivelUrgencia;
 import com.algaworks.especialista.notificacao.Notificador;
 import com.algaworks.especialista.notificacao.TipoDoNotificador;
 
-@Component
+//@Component
 public class AtivacaoClienteService {
 
 	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired
 	private Notificador notificador;
-	
+
+//	@PostConstruct
+	public void init() {
+		System.out.println("INIT " + notificador);
+	}
+
+//	@PreDestroy
+	public void destroy() {
+		System.out.println("DESTROY ");
+	}
+
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
 
 }
