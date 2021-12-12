f453d42a591a3bf07396306d700dba37515e60a1
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-12-12 09:48:54 -0300

    Adicionado conf usando profile

--- src/main/java/com/algaworks/especialista/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/especialista/notificacao/NotificadorEmail.java
@@ -1,12 +1,18 @@
 package com.algaworks.especialista.notificacao;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.especialista.modelo.Cliente;
 
+@Profile("prod")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
+
+	public NotificadorEmail() {
+		System.out.println("Notificador Email Real");
+	}
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
