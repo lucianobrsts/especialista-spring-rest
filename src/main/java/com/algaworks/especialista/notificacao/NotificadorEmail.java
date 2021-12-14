bb69f67628d21a66b6422d34fd99732bd2b058de
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-12-13 23:17:10 -0300

    Adicionado conf variaveis via arquivo properties

--- src/main/java/com/algaworks/especialista/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/especialista/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.algaworks.especialista.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.especialista.modelo.Cliente;
@@ -7,9 +8,18 @@ import com.algaworks.especialista.modelo.Cliente;
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
+	
+	@Value("${notificador.email.host-servidor}")
+	private String host;
+	
+	@Value("${notificador.email.porta-servidor}")
+	private Integer porta;
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Host: " + host);
+		System.out.println("Porta: " + porta);
+		
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
