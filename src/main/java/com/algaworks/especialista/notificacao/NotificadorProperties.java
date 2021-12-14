8ba2aec726e7254c45eab7fcf05bc91de8ee42ec
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-12-13 23:24:03 -0300

    Adicionado dependencia sprint boot processor ao pom

--- src/main/java/com/algaworks/especialista/notificacao/NotificadorProperties.java
+++ src/main/java/com/algaworks/especialista/notificacao/NotificadorProperties.java
@@ -6,8 +6,24 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties("notificador.email")
 public class NotificadorProperties {
-	
+
 	private String hostServidor;
 	private Integer portaServidor;
+
+	public String getHostServidor() {
+		return hostServidor;
+	}
+
+	public void setHostServidor(String hostServidor) {
+		this.hostServidor = hostServidor;
+	}
+
+	public Integer getPortaServidor() {
+		return portaServidor;
+	}
+
+	public void setPortaServidor(Integer portaServidor) {
+		this.portaServidor = portaServidor;
+	}
 
 }
